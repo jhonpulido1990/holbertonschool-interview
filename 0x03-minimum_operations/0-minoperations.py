c3af9a79c53160b6e310bad64e66932f4d353211
jhonpulido1990 <3825@holbertonstudents.com>
2022-08-25 13:56:08 -0500

    fixed operation

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 '''Your text editor can execute only two operations in this
 file: Copy All and Paste. Given a number n, write a method
-that calculates the fewest number of operations needed to
-result in exactly n H characters in the file.'''
+that calculates the fewest number of operations needed toresult in exactly n H characters in the file.'''
 
 def minOperations(n):
 	'''copy and paste counts as 2 actions'''
 	if n <= 1:
 		return 0
-	num = 0
+
+	num = n
 	div = 2
-	operations = n
+	operations = 0
+
 	while num > 1:
 		if num % div == 0:
 			num = num / div
