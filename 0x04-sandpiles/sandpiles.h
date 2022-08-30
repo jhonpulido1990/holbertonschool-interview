545d59bcca279fa92557f82e5c103e1f6c1e0014
jhonpulido1990 <3825@holbertonstudents.com>
2022-08-29 22:29:12 -0500

    header

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_sandpile(int grid1[3][3], int grid2[3][3]);
+int valid_sandpaile(int grid1[3][3]);
+void funt_sandpaile(int grid1[3][3]);
+void print_sandpile(int grid[3][3]);
+
+#endif /* SANDPILES_H */
