65263ece346e5f568b7c6344aa26f64b63a0fc1d
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-06 14:47:51 -0500

    add task

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: likend list
+ * Return: 0 or 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int count = 0, pos_end = 0, pos_init = 0, array[1024];
+	const listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		array[count] = current->n;
+		current = current->next;
+		count++;
+	};
+	
+	pos_end = count - 1;
+	count = count / 2;
+	while (pos_init <= count)
+	{
+		if (array[pos_init] != array[pos_end])
+			return (0);
+		pos_end--, pos_init++;
+	}
+	return (1);
+}
