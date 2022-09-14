d53c059e23d38d51291602ffef0925d444cf373a
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-14 10:53:11 -0500

    chenge variable uppercase

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,35 +10,35 @@ statecode = {"200": 0, "301": 0, "400": 0,
              "401": 0, "403": 0, "404": 0,
              "405": 0, "500": 0}
 
-SIZEFILE = 0
+size = 0
 
 
 def print_list():
     """
     Total file size:
     """
-    print("File size: {}".format(SIZEFILE))
+    print("File size: {}".format(size))
     for key in sorted(statecode.keys()):
         if statecode[key]:
             print("{}: {}".format(key, statecode[key]))
 
 
 if __name__ == "__main__":
-    COUNT = 0
+    count = 0
     try:
         for line in stdin:
             try:
                 getdata = line.split()
-                SIZEFILE += int(getdata[8])
+                size += int(getdata[8])
                 key_statucode = getdata[7]
                 if key_statucode in statecode:
                     statecode[key_statucode] += 1
             except:
                 pass
-            if COUNT == 9:
+            if count == 9:
                 print_list()
-                COUNT = -1
-            COUNT += 1
+                count = -1
+            count += 1
     except KeyboardInterrupt:
         print_list()
         raise
