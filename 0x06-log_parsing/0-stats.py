83beb58c5467524a16d9ef1ae970f0853cb1d8a7
jhonpulido1990 <3825@holbertonstudents.com>
2022-09-14 10:35:29 -0500

    cahnge split in line 31

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 """
-Module that parses a log and prints stats to stdout
+Write a script that reads stdin line
+by line and computes metrics:
 """
-from sys import stdin
+import sys
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+SIZEFILE = 0
 
-size = 0
+statecode = {"200": 0, "301": 0, "400": 0,
+             "401": 0, "403": 0, "404": 0,
+             "405": 0, "500": 0}
 
 
-def print_stats():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
+def print_list():
+    """
+    Total file size: File size: <total size>
+    format: <status code>: <number>
+    """
+    print("File size: {:d}".format(SIZEFILE))
+    for key in sorted(statecode.keys()):
+        if statecode[key]:
+            print("{}: {}".format(key, statecode[key]))
 
 
 if __name__ == "__main__":
-    count = 0
+    COUNT = 0
     try:
-        for line in stdin:
+        for line in sys.stdin:
             try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
+                getdata = line.split()
+                SIZEFILE += int(getdata[8])
+                key_statucode = getdata[7]
+                if key_statucode in statecode:
+                    statecode[key_statucode] += 1
             except:
                 pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
+            if COUNT == 9:
+                print_list()
+                COUNT = -1
+            COUNT += 1
     except KeyboardInterrupt:
-        print_stats()
+        print_list()
         raise
-    print_stats()
+    print_list()
