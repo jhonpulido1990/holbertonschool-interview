7b2e06e4cb19042058fa1b54a27423bee0d8f0bb
jhonpulido1990 <3825@holbertonstudents.com>
2022-11-28 17:12:35 -0500

    add fixed

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,38 +1,31 @@
 #!/usr/bin/node
 
+// Write a script that prints all characters of a Star Wars movie:
+// The first positional argument passed is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name per line in the same order as the “characters” list in the /films/ endpoint
+// You must use the Star wars API
+// You must use the request module
+
 const request = require('request');
-const urlApi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-const promisifiedRequest = function (options) {
-  return new Promise((resolve, reject) => {
-    request(options, (error, response, body) => {
-      if (response) {
-        return resolve(response);
-      }
-      if (error) {
-        return reject(error);
-      }
-    });
-  });
-};
-
-request(urlApi, function (error, response, body) {
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, async function (error, response, body) {
   if (error) {
-    console.error(error); // Print the error if one occurred
-    return;
-  }
-  const jsonRes = JSON.parse(body);
-  const charUrl = jsonRes.characters;
-  (async function () {
-    for (let i = 0; i < charUrl.length; i++) {
-      const options = {
-        url: charUrl[i],
-        method: 'GET'
-      };
-
-      const response = await promisifiedRequest(options);
-
-      console.log(JSON.parse(response.body).name);
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(character, (error, res, html) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
     }
-  })();
+  }
 });
